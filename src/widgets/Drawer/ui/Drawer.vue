588a0bd93de1de914f37bb0c2a69c0669f6208c1
Denis341414 <denismanshin2@gmail.com>
2025-06-09 09:47:00 +0300

    del switchTheme

--- src/widgets/Drawer/ui/Drawer.vue
+++ src/widgets/Drawer/ui/Drawer.vue
@@ -4,7 +4,6 @@ import { MenuBar } from "@features/MenuBar";
 import { useDrawerStore } from "../model";
 import { storeToRefs } from "pinia";
 import ButtonDrawer from "./button-drawer.vue";
-import { SwitchTheme } from "../utils";
 
 const { visible } = storeToRefs(useDrawerStore());
 </script>
@@ -17,12 +16,6 @@ const { visible } = storeToRefs(useDrawerStore());
       class="drawer relative !bg-green-700 opacity-[0.9] !shadow-2xl !shadow-cyan-500/50 !p-6 gap-10 text-3xl"
     >
       <MenuBar />
-      <Button
-        @click="SwitchTheme"
-        class="flex w-full !justify-start !border-4 !mt-6 items-center cursor-pointer text-surface-700 !transition-all duration-300 !px-10 !py-2 dark:text-surface-0 hover:!bg-gray-700"
-      >
-        Switch Theme
-      </Button>
     </Drawer>
 
     <ButtonDrawer />
